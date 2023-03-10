8f8f81e934b07af16b25b7a8c918ba8b37f8341e
Carrie <carrieyu@DESKTOP-N1A0LCS.localdomain>
2023-03-10 04:13:49 -0500

    completed

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
-
-
 #include "funcs.h"
+#include "caesar.h"
+#include "vigenere.h"
+#include "decryption.h"
 
 int main()
 {
-  return 0;
+  //Task B; Caesar cipher encryption
+  std:: cout << encryptCaesar("Way to Go!", 5) << std:: endl; ////output: "Bfd yt Lt!"
+
+  //Task C; Vigenere cipher encryption
+//encryptVigenere(std:: string plaintext, std:: string keyword);
+  std:: cout << encryptVigenere("Hello, World!", "cake") << std:: endl;
+
+//Task D; Decryption
+//decryptCaesar(ciphertext, rshift) == plaintext
+//decryptVigenere(ciphertext, keyword) == plaintext
+  std:: cout << decryptCaesar("Rovvy, Gybvn!", 10) << std:: endl;
+  std:: cout << decryptVigenere("Jevpq, Wyvnd!", "cake") << std:: endl;
+
 }
