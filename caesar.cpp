63fb089cc1ca3547ab3cc8e96996760bec1592e7
Carrie <carrieyu@DESKTOP-N1A0LCS.localdomain>
2023-04-16 22:13:32 -0400

    updated

--- caesar.cpp
+++ caesar.cpp
@@ -4,42 +4,27 @@
 
 // A helper function to shift one character by rshift
 char shiftChar(char c, int rshift){
-
-  //initial
-  /*
-  if((c > 64) && (c < 91)){ //upper alphabet
-    c += rshift;
-  }
-  else{
-    if((c > 96) && (c < 123)){ //lowercase alphabet
-    c += rshift;
-      }
+  
+  if((c > 64 && c < 91)){ //character values from A to Z
+    if(c+rshift <= 90){
+      c += rshift; //adding rshift to the current character value
     }
-  return c;
-*/
-
-   if((c > 64 && c < 91)){ //character values from A to Z
-    c += rshift; //adding rshift to the current character value
-    if(c <= 64){
-      c+=26;
-    }
-    else if(c >= 91){
-      c-=26;
+    else if(c+rshift >= 91){
+    c += rshift - 26;
     }
   }
   else{
     if((c > 96 && c < 123)){ //character values from a to z
-    c+= rshift;
-    if(c <= 96){
-      c+=26;
-    }
-    else if(c >= 123){
-      c-=26;
+      if(c+rshift <= 122){
+        c += rshift; //adding rshift to the current character value
+      }
+      else if(c+rshift >= 123){
+        c+= rshift - 26;
+     }
     }
   }
-}
-return c;
-  
+  return c;
+
 }
 
 // Caesar cipher encryption
@@ -53,6 +38,7 @@ std:: string encryptCaesar(std:: string plaintext, int rshift){
   return result;
   
 }
+
 
 
 
