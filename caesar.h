420de0aff55d7623e1a124ec9a44df9810b515ae
Carrie <carrieyu@DESKTOP-N1A0LCS.localdomain>
2023-04-17 03:53:02 -0400

    updated

--- caesar.h
+++ caesar.h
@@ -1,5 +1,7 @@
+#include <iostream>
 #pragma once
 
 //Task B; Caesar cipher encryption
 char shiftChar(char c, int rshift);
 std:: string encryptCaesar(std:: string plaintext, int rshift);
+
--- decryption.h
+++ decryption.h
@@ -1,4 +1,9 @@
+#include <iostream>
+#include <vector>
 #pragma once
 
 std:: string decryptCaesar(std:: string ciphertext, int rshift);
 std:: string decryptVigenere(std:: string ciphertext, std:: string keyword);
+double stringFrequency(char c, std:: string str);
+double distance(std:: vector <double> array1, std:: vector <double> array2);
+std:: string solve(std:: string encrypted_string);
